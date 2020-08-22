14804b66f5f06aef9b9f0642a66ce6ae39261ad4
Mark <si3rr4wow@gmail.com>
2020-08-22 22:02:52 +0100

    spinny boys

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,32 @@ function App() {
   return (
     <>
       <h1>threejs render test</h1>
-      <Canvas colorManagement style={{ background: "#171717" }}>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Suspense fallback={Loading}>
-          <D4 position={[0, 0, 0]}/>
-        </Suspense>
-      </Canvas>
+      <div style={{ height: '75%' }}>
+        <Canvas colorManagement style={{ background: "#171717" }}>
+          <ambientLight color={0xf7c481}/>
+          <pointLight position={[-20, 0, 10]} intensity={2} color={0x81f7f7}/>
+          <pointLight position={[20, 0, 10]} intensity={2} color={0xf781c2}/>
+          <Suspense fallback={Loading}>
+            <D4 position={[-4, 0, 0]}/>
+            <D4 position={[-2, 0, 0]}/>
+            <D4 position={[0, 0, 0]}/>
+            <D4 position={[2, 0, 0]}/>
+            <D4 position={[4, 0, 0]}/>
+
+            <D4 position={[-4, 2, 0]}/>
+            <D4 position={[-2, 2, 0]}/>
+            <D4 position={[0, 2, 0]}/>
+            <D4 position={[2, 2, 0]}/>
+            <D4 position={[4, 2, 0]}/>
+
+            <D4 position={[-4, -2, 0]}/>
+            <D4 position={[-2, -2, 0]}/>
+            <D4 position={[0, -2, 0]}/>
+            <D4 position={[2, -2, 0]}/>
+            <D4 position={[4, -2, 0]}/>
+          </Suspense>
+        </Canvas>
+      </div>
     </>
   );
 }
--- client/src/three/components/d4.jsx
+++ client/src/three/components/d4.jsx
@@ -1,30 +1,115 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useLoader, useFrame } from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const D4 = props => {
-  const { nodes } = useLoader(GLTFLoader, "models/d4.glb");
+const acceleration = 0.001
 
+const isStationaryWithImpulse = state => (
+  state.accelerating === true &&
+  state.spinAcceleration === 0
+)
+
+const isStillAccelerating = state => (
+  state.spinAcceleration > -0.15 &&
+  state.spinAcceleration < 0.15 &&
+  state.accelerating === true
+)
+
+const isInStopRegion = state => (
+  state.spinAcceleration > -0.002 &&
+  state.spinAcceleration < 0.002
+)
+
+const applyRotation = (mesh, setState) => {
+  setState(s => {
+    let nextSpinAcceleration
+    let nextAccelerating = s.accelerating
+    if(isStationaryWithImpulse(s)) {
+      nextSpinAcceleration = s.spinAcceleration + 4 * acceleration
+    } else if(isStillAccelerating(s) && !isInStopRegion(s)) {
+      nextSpinAcceleration = s.spinAcceleration + acceleration
+    } else if(isInStopRegion(s)) {
+      nextSpinAcceleration = 0
+    } else {
+      nextSpinAcceleration = s.spinAcceleration - acceleration
+      nextAccelerating = false
+    }
+
+    if(nextSpinAcceleration) {
+      mesh.current.rotation.x = (
+        mesh.current.rotation.y
+      ) += nextSpinAcceleration
+    }
+    return {
+      ...s,
+      accelerating: nextAccelerating,
+      spinAcceleration: nextSpinAcceleration
+    }
+  })
+}
+
+const applyScaling = (mesh, state) => {
+  if(state.hovered && mesh.current.scale.length() < 1.2) {
+    mesh.current.scale.x =
+    mesh.current.scale.y =
+    mesh.current.scale.z += 0.03
+  }
+  else if(!state.hovered && mesh.current.scale.length() > 1) {
+    mesh.current.scale.x =
+    mesh.current.scale.y =
+    mesh.current.scale.z -= 0.03
+  }
+}
+
+const D4 = props => {
+  const [state, setState] = useState({
+    hovered: false,
+    spinAcceleration: 0,
+    accelerating: false
+  })
+  const { nodes } = useLoader(GLTFLoader, "models/d4.glb");
   const mesh = useRef()
+
+  const handleClick = () => {
+    setState(s => ({
+      ...s,
+      accelerating: true
+    }))
+  }
+
+  const handlePointerOver = () => {
+    setState(s => ({
+      ...s,
+      hovered: true
+    }))
+  }
+
+  const handlePointerOut = () => {
+    setState(s => ({
+      ...s,
+      hovered: false
+    }))
+  }
 
   useFrame(() => {
     if(!mesh.current) { return }
-    return mesh.current.rotation.x
-      = mesh.current.rotation.y
-      += 0.05;
+    applyRotation(mesh, setState)
+    applyScaling(mesh, state)
   })
 
   return (
     <mesh visible
       {...props}
       ref={mesh}
-      geometry={nodes.Circle.geometry}>
+      geometry={nodes.Circle.geometry}
+      onClick={handleClick}
+      onPointerOver={handlePointerOver}
+      onPointerOut={handlePointerOut} >
       <meshStandardMaterial
         attach="material"
-        color="white"
-        roughness={0.3}
-        metalness={0.3}
-      />
+        color={0x888888}
+        roughness={0.7}
+        metalness={0.3} />
     </mesh>
   );
 }
